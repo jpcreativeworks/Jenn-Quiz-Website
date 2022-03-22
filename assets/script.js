f9ff56700fb63a19dbb842d675175fba105bfa61
jpcreativeworks <jp.graphics.011@gmail.com>
2022-03-22 16:41:29 -0700

    added css elmts and js variables

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let mainQuestion1 = ["What element of Web Coding would be considered the muscles?"];
+let mainAnswer1 = ["JavaScript", "HTML", "CSS"];
+let mainQuestion2 = ["Is the following statement print the numbers from 1 to 10?" <br> "for(let i = 1; i <10; i++)"];
+let mainAnswer2 = [true, false, null];
+let mainQuestion3 = ["How many actual lines are there in this list?" <br> "<ol><li>Apples, Pizza, Drycleaning</li></ol>"];
+let mainAnswer3 = ["3", "1", "2"];
+
